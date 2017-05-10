7dcc0483c174a9383580e25047c5ee6341f8a8b6
michelsm <michels.m@husky.neu.edu>
2017-05-10 11:14:51 -0400

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
+// stream it verbaten back to the client
 app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
