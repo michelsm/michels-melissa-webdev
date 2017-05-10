b2400c2880ad2de55ef5631e1c0f0670d4a282cf
michelsm <michels.m@husky.neu.edu>
2017-05-10 12:51:56 -0400

    second commit

--- test/app.js
+++ test/app.js
@@ -7,11 +7,10 @@ module.exports = function(app)
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+        var username = process.env.MLAB_USERNAME;
+	var password = process.env.MLAB_PASSWORD;
+        connectionString = 'mongodb://' + username + ':' + password;
+	connectionString += '@ds137101.mlab.com:37101/heroku_bxszhj3x';
     }
 
     var mongoose = require("mongoose");
